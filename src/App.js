2a1b79121eacf8061475d2974b0aa00ac4551e2a
koderoom <koderoom.dev@gmail.com>
2021-01-19 17:08:40 +0530

    workign with background images.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import logo from "./logo.svg";
-import imgMountain from "./images/mountain.jpg";
-
 import "./App.css";
 
 /**
@@ -8,18 +5,9 @@ import "./App.css";
  * Image can be download and part of your project. as a file.
  */
 function App() {
-  const imageUrl = "https://picsum.photos/200/300";
-
   return (
-    <div>
-      <h1>Image As URL</h1>
-      {/** IMAGE AS URL */}
-      <img src="https://picsum.photos/200/300" alt="" />
-      <img src={imageUrl} alt="" />
-
-      {/** Image as file */}
-      <h1>Image As Downloaded File</h1>
-      <img src={imgMountain} alt="" />
+    <div className="my-bg-image-1">
+      <div>Hello World</div>
     </div>
   );
 }
