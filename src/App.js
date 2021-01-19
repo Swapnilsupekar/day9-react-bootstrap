20cb96e1dd5c7179b4203d7be93936a9c88d63ab
koderoom <koderoom.dev@gmail.com>
2021-01-19 17:57:46 +0530

    carousel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Carousel } from "react-bootstrap";
 
 function App() {
   let list = [
@@ -19,22 +20,51 @@ function App() {
 
         {/** Can I Add Children Here */}
         <div className="col-12 col-md-6 ">
-          {list.map((item, index) => (
-            <div
-              key={index}
-              className="d-flex justify-content-center align-items-center text-light mb-2 bg-secondary"
-              style={{ height: "250px", fontSize: "2rem" }}
-            >
+          <Carousel>
+            <Carousel.Item>
               <img
-                src={item}
-                style={{
-                  height: "100%",
-                  width: "100%",
-                  objectFit: "cover",
-                }}
+                className="d-block w-100"
+                src="https://picsum.photos/200/300"
+                alt="First slide"
               />
-            </div>
-          ))}
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis
+                  interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://picsum.photos/200/301"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://picsum.photos/200/303"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </div>
 
         <div className="d-none d-md-block col-md-3 ">COL3</div>
