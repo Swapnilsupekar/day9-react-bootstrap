46574fc4a392e7b79fb891011496a2d22371d036
koderoom <koderoom.dev@gmail.com>
2021-01-19 16:57:14 +0530

    working with images.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import imgMountain from "./images/mountain.jpg";
 
+import "./App.css";
+
+/**
+ * IMAGE can be a URL
+ * Image can be download and part of your project. as a file.
+ */
 function App() {
+  const imageUrl = "https://picsum.photos/200/300";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Image As URL</h1>
+      {/** IMAGE AS URL */}
+      <img src="https://picsum.photos/200/300" alt="" />
+      <img src={imageUrl} alt="" />
+
+      {/** Image as file */}
+      <h1>Image As Downloaded File</h1>
+      <img src={imgMountain} alt="" />
     </div>
   );
 }
