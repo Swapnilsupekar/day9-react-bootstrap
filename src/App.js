788504d4b46de76c26a22bb681da820ec0435a09
koderoom <koderoom.dev@gmail.com>
2021-01-19 17:29:25 +0530

    responsinveness

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="my-bg-image-1">
-      <div>Hello World</div>
+    <div className="container-fluid">
+      {/** First Row  */}
+      <div className="row bg-primary">
+        <div className="d-none d-md-block col-md-3  bg-info">COL 1</div>
+        <div className="col-12 col-md-6 bg-success">COL 2</div>
+        <div className="d-none d-md-block col-md-3 bg-danger">COL 3</div>
+      </div>
     </div>
   );
 }
